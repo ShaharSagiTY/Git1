ad20fab47bd70453dfbf4de0c2a8f9c4da0220b5
ShaharSagiTY <161124051+ShaharSagiTY@users.noreply.github.com>
2024-08-05 14:33:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 alert("Hello!");
 document.querySelector("#PageTitle").innerHTML = "This is a New Title";
 document.querySelector("#HelloTest").innerHTML = "Hello new world";
+alert(document.querySelector("#test123").innerHTML)
