b7ffe4d69705fdc8480c1b74aca9dc8d7905def1
ShaharSagiTY <161124051+ShaharSagiTY@users.noreply.github.com>
2024-08-05 15:00:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 alert("Hello!");
 document.querySelector("#PageTitle").innerHTML = "This is a New Title";
 document.querySelector("#HelloTest").innerHTML = "Hello new world";
-const testVar = window.parent.document.getElementById("test123");
-console.log(testVar.innerHTML);
-alert(testVar.innerHTML);
+const testVar = window.parent.document.getElementById("test123").innerHTML;
+console.log(testVar);
+alert(testVar);
+document.querySelector("#Secondary").innerHTML = testVar;
